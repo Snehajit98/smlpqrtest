f6c46adc31ccf3d55fc3918494cae57eed74f8e5
Snehajit98 <33743991+Snehajit98@users.noreply.github.com>
2023-04-22 14:05:46 +0530

    Add files via upload

--- SQLinsert.js
+++ SQLinsert.js
@@ -3,7 +3,8 @@ var db = window.openDatabase("itemDB","1.0","itemDB",65535);
 var sql =""
 
 $(function(){
-    loadData();
+    loadData()
+    //location.reload(true);
     //creating a db
     $("#create").click(function(){
     db.transaction(function(transaction){
@@ -14,6 +15,7 @@ $(function(){
             transaction.executeSql(sql,undefined,
             function(){alert("Database created!");},
             function(){alert("Database already exists");})
+            loadData();
         });
     });
     
@@ -26,6 +28,7 @@ $(function(){
             function(){alert("table deleted")},
             function(){alert("No tables found to delete");});
             $("#itemlist").children().remove();
+
         });
     });
 
@@ -33,7 +36,7 @@ $(function(){
     $("#insert").click(function(){
     var item = $("#instrument").val()
     //var item = $("#result").val()
-    console.log($("#result").val())
+    //console.log($("#result").val())
     var qty = $("#quantity").val();
     db.transaction(function(transaction){
         // alert("item added successfully");
@@ -42,8 +45,10 @@ $(function(){
         function(){alert("item added successfully")},
         function(transaction,err){alert(//err.message//
         "No Database Found. Create a database first")});
-        location.reload(true);
+        loadData();
+        
     });
+    // location.reload(true);
     });
 
     //fetching records of available material
@@ -65,8 +70,8 @@ function loadData(){
                    // $("#itemlist").append('<tr><td>'+id+'</td><td>'+item+'</td><td>'+quantity+'</td><td><button href ="#" button type="button" class="btn-danger" deleteitem data-id="'+id+
                    // '">DELETE</button> <button href="#" type="button" class="btn-primary" id="find">FIND</button></td></tr>');
                    $("#itemlist").append(`<tr><td>`+id+`</td><td>`+item+`</td><td>`+quantity+`</td>
-                   <td><button type="button" id=`+button_id+` class="btn-danger"><span class="bi bi-trash-fill" style="font-size:1rem"></span> Delete</button></td>
-                   <td><button type="button" id=`+button_id+` class="btn-primary"><span class="bi bi-pencil-square" style="font-size:1rem"></span> Edit</button></td>
+                   <td><button type="button" id=`+button_id+` class="btn btn-danger"><span class="bi bi-trash-fill" style="font-size:1rem"></span> Delete</button></td>
+                   <td><button type="button" id=`+button_id+` class="btn btn-primary"><span class="bi bi-pencil-square" style="font-size:1rem"></span> Edit</button></td>
                    </tr>`);
                 }
 
@@ -94,7 +99,7 @@ function loadData(){
 
 
 
-
+                  
                 
 
 
@@ -107,9 +112,50 @@ function loadData(){
         }
         );
     });
+    }
 
+    $("#search_btn").click(function(){
+        load_search();
+    });
+
+
+    function load_search(){
+        $("#search_item").children().remove();
+        var search_word = $("#search_text").val()
+
+       if(search_word){
+        db.transaction(function(transaction){
+            // sql = "SELECT * FROM items WHERE item='2053A3058' ORDER BY id ASC"; //THIS IS PERFECT. BUT LETS TRY SOMETHING ELSE.
+            sql = "SELECT * FROM items WHERE item LIKE '%"+search_word+"%' ORDER BY id ASC";
+            transaction.executeSql(sql, undefined,function(transaction,result){
+                if(result.rows.length){
+                    for(var i=0;i<result.rows.length;i++){
+                        var row = result.rows.item(i);
+                        var id = row.id;
+                        var item = row.item;
+                        var quantity = row.quantity;
+                        var button_id = "delete"+id;
+                       $("#search_item").append(`<tr><td>`+id+`</td><td>`+item+`</td><td>`+quantity+`</td>
+                        <td><button type="button" id=`+button_id+` class="btn btn-danger"><span class="bi bi-trash-fill" style="font-size:1rem"></span> Delete</button></td>
+                      
+                       </tr>`);
+                      // $("#search_item").remove()
+
+                    }}
+                    else{ $("#search_item").append("No Data Found")}
+                },function(transaction,err){
+                        alert(/*err.message*/"No Database Found");
+                    });
+
+            });
+            alert("searching..."+search_word)
+        }
+        else {
+            alert("Search Input is empty")
+        }
 
     }
+
 })
 
 
