5acc351a59ebb5a44455bc3ba6f528fc8be90c8c
Snehajit98 <33743991+Snehajit98@users.noreply.github.com>
2023-05-25 08:29:59 +0530

    Add files via upload

--- SQLinsert.js
+++ SQLinsert.js
@@ -11,11 +11,12 @@ $(function(){
     sql = "CREATE TABLE items"+
             "(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"+
             "item VARCHAR(100) NOT NULL,"+ //will use this "item" as a key(as #item) in insert function
+            "desc VARCHAR(100) NOT NULL,"+
             "quantity INTEGER(5) NOT NULL)"; //will use this "quantity" as another key(as #quantity) in insert function
             transaction.executeSql(sql,undefined,
             function(){alert("Database created!");},
             function(){alert("Database already exists");})
-            loadData();
+            location.reload()
         });
     });
     
@@ -37,46 +38,47 @@ $(function(){
     var item_input = $("#instrument").val()
     var qty_input = $("#quantity").val();
 
-
+    //insert_function(item_input,qty_input);
     db.transaction(function(transaction){
-       // sql = "SELECT * FROM items WHERE item="+item_input+" ORDER BY id ASC"; //THIS IS PERFECT. BUT LETS TRY SOMETHING ELSE.
-       sql = "SELECT * FROM items WHERE item LIKE '%"+item_input+"%' ORDER BY id ASC";
-        transaction.executeSql(sql, undefined,function(transaction,result){
-            if(result.rows.length){
-                for(var i=0;i<result.rows.length;i++){
-                    var row = result.rows.item(i);
-                    var id = row.id;
-                    var quantity = Number(row.quantity)+Number(qty_input);
+        // sql = "SELECT * FROM items WHERE item="+item_input+" ORDER BY id ASC"; //THIS IS PERFECT. BUT LETS TRY SOMETHING ELSE.
+        sql = "SELECT * FROM items WHERE item LIKE '%"+item_input+"%' OR desc LIKE '%"+item_input+"%' ORDER BY id ASC";
+         transaction.executeSql(sql, undefined,function(transaction,result){
+             if(result.rows.length){
+                 for(var i=0;i<result.rows.length;i++){
+                     var row = result.rows.item(i);
+                     var id = row.id;
+                     var quantity = Number(row.quantity)+Number(qty_input);
+    
+                     db.transaction(function(transaction){
+                         sql = "UPDATE items SET quantity = "+quantity+" WHERE id ="+id+""
+                         transaction.executeSql(sql,[])
+                         });
+                     
+                     loadData();
+    
+                 }}
+                 else{
+    
+    
+                     db.transaction(function(transaction){
+    
+                         sql = "INSERT INTO items(item,quantity) VALUES(?,?)";
+                         transaction.executeSql(sql,[item_input,qty_input],
+                         function(){alert("item added successfully")},
+                         function(transaction,err){alert(//err.message//
+                         "No Database Found. Create a database first")});
+                         loadData();
+                         
+                     });
+    
+    
+                 }
+             },function(transaction,err){
+                     alert(err.message);
+                 });
+    
+         });
 
-                    db.transaction(function(transaction){
-                        sql = "UPDATE items SET quantity = "+quantity+" WHERE id ="+id+""
-                        transaction.executeSql(sql,[])
-                        });
-                    
-                    loadData();
-
-                }}
-                else{
-
-
-                    db.transaction(function(transaction){
-
-                        sql = "INSERT INTO items(item,quantity) VALUES(?,?)";
-                        transaction.executeSql(sql,[item_input,qty_input],
-                        function(){alert("item added successfully")},
-                        function(transaction,err){alert(//err.message//
-                        "No Database Found. Create a database first")});
-                        loadData();
-                        
-                    });
-
-
-                }
-            },function(transaction,err){
-                    alert(err.message);
-                });
-
-        });
 
 
     });
@@ -106,7 +108,7 @@ function loadData(){
    
                     htmlData += `<tr><td>`+id+`</td><td>`+item+`</td><td>`+quantity+`</td>
                         <td><button type="button" id=`+button_id+` class="btn btn-danger"><span class="bi bi-trash-fill" style="font-size:1rem"></span> Delete</button></td>
-                       <td><button type="button" id=`+button_id+` class="btn btn-primary"><span class="bi bi-pencil-square" style="font-size:1rem"></span> Edit</button></td>
+                     <td><button type="button" id=`+button_id+` class="btn btn-primary"><span class="bi bi-pencil-square" style="font-size:1rem"></span> Edit</button></td>
                         </tr>`;
 
                 }
@@ -128,7 +130,9 @@ function loadData(){
                             //   alert(qty_del_update)
                             sql = "UPDATE items SET quantity = "+ qty_del_update+" WHERE id ="+i
                             transaction.executeSql(sql,[])
-                            location.reload();}
+                            $("#fetch").click()
+                            
+                        }
                             else{
                                 alert("This item is already 0")
                             }
@@ -136,11 +140,12 @@ function loadData(){
                             
                     })})
                     
-              })(i);
-            
+              })(i);  
 
             }
             else{
+                $("#itemlist").empty()
+                
                 $("#itemlist").append("<tr><td colspan='3' align ='center'> No Items Found</td><tr>");
             }
         },function(transaction,err){
@@ -148,26 +153,23 @@ function loadData(){
         }
         );
     });
+  
     }
 
+
+
     $("#search_btn").click(function(){
-        load_search();
-    });
-
-
-  
-
-
-
-
-    function load_search(){
-        $("#search_item").children().remove();
+        
         var search_word = $("#search_text").val()
-
-       if(search_word){
-        db.transaction(function(transaction){
+        load_search(search_word);
+        
+    });
+    function load_search( search_word ){
+        $("#search_item").children().remove();
+        if(search_word){
+            db.transaction(function(transaction){
             // sql = "SELECT * FROM items WHERE item='2053A3058' ORDER BY id ASC"; //THIS IS PERFECT. BUT LETS TRY SOMETHING ELSE.
-            sql = "SELECT * FROM items WHERE item LIKE '%"+search_word+"%' ORDER BY id ASC";
+            sql = "SELECT * FROM items WHERE item LIKE '%"+search_word+"%' OR desc ORDER BY id ASC";
             transaction.executeSql(sql, undefined,function(transaction,result){
                 if(result.rows.length){
                     for(var i=0;i<result.rows.length;i++){
@@ -183,9 +185,13 @@ function loadData(){
                       // $("#search_item").remove()
 
                     }}
-                    else{ $("#search_item").append("No Data Found")}
+                    else{ 
+                       $("#search_item").empty()
+                      
+                        $("#search_item").append("No Data Found")}
+
                 },function(transaction,err){
-                        alert(/*err.message*/"No Database Found");
+                        alert(/*err.message*/"No Data Found");
                     });
 
             });
@@ -198,6 +204,3 @@ function loadData(){
     }
 
 })
-
-
-
